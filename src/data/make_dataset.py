5e852fa3399a39b55c2d1577a24f320de80a5280
hgamarro <hgamarro@gmail.com>
2021-04-21 01:48:58 -0400

    Raw data processing for spacenet data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,19 +2,24 @@
 import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+from GeoUtilities import DataConverter
 
-
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+def main(project_dir):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
-
+    
+    
+    input_path = str(project_dir) + '/data/raw/AOI_2_Vegas_Train'
+    output_path = str(project_dir) + '/data/processed/Vegas/train'
+    val_path = str(project_dir) + '/data/processed/Vegas/val'
+    
+    converter = DataConverter.SpaceNetDataConverter(input_path, output_path, 2, "Vegas")
+    converter.convertAllToInput()
+    
+    DataConverter.train_val_split(output_path, val_path, 0.8)
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -22,9 +27,4 @@ if __name__ == '__main__':
 
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    main(project_dir)
