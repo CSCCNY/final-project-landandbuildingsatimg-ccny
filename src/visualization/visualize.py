80d4981ff0697b73708c148b733d83baa31e1b1e
saidmejiam <71112369+saidmejiam@users.noreply.github.com>
2021-04-23 00:40:25 -0400

    History Functions

---
+++ src/visualization/visualize.py
@@ -0,0 +1,25 @@
+### History Functions
+def plot_history(history):
+       
+    acc = history.history['categorical_accuracy']
+    val_acc = history.history['val_categorical_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(acc)
+    plt.plot(val_acc)
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    
+    plt.subplot(1, 2, 2)
+    plt.plot(loss)
+    plt.plot(val_loss)
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    
+    plt.show();
+    
