c14d0cf468ff2d70673660372707b4853d66f137
Juan Montoya <jmontoy004@citymail.cuny.edu>
2021-04-28 12:18:12 -0400

    Mask Converter Function Added to src

---
+++ src/data/GeoUtilities/MaskCoverter.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+def RGB_mapping_to_class(label):
+#   Convert RGB mask to categorical array.
+    l, w = label.shape[0], label.shape[1]
+    classmap = np.zeros(shape=(l, w))
+    indices = np.where(np.all(label == (0, 255, 255), axis=-1))
+    classmap[indices[0].tolist(), indices[1].tolist()] = 1
+    indices = np.where(np.all(label == (255, 255, 0), axis=-1))
+    classmap[indices[0].tolist(), indices[1].tolist()] = 2
+    indices = np.where(np.all(label == (255, 0, 255), axis=-1))
+    classmap[indices[0].tolist(), indices[1].tolist()] = 3
+    indices = np.where(np.all(label == (0, 255, 0), axis=-1))
+    classmap[indices[0].tolist(), indices[1].tolist()] = 4
+    indices = np.where(np.all(label == (0, 0, 255), axis=-1))
+    classmap[indices[0].tolist(), indices[1].tolist()] = 5
+    indices = np.where(np.all(label == (255, 255, 255), axis=-1))
+    classmap[indices[0].tolist(), indices[1].tolist()] = 6
+    indices = np.where(np.all(label == (0, 0, 0), axis=-1))
+    classmap[indices[0].tolist(), indices[1].tolist()] = 0
+    #     plt.imshow(colmap)
+    #     plt.show()
+    return classmap
+
+
+def classToRGB(label):
+#   Convert categorical array to RGB image.
+    l, w = label.shape[0], label.shape[1]
+    colmap = np.zeros(shape=(l, w, 3)).astype(np.float32)
+    indices = np.where(label == 1)
+    colmap[indices[0].tolist(), indices[1].tolist(), :] = [0, 255, 255]
+    indices = np.where(label == 2)
+    colmap[indices[0].tolist(), indices[1].tolist(), :] = [255, 255, 0]
+    indices = np.where(label == 3)
+    colmap[indices[0].tolist(), indices[1].tolist(), :] = [255, 0, 255]
+    indices = np.where(label == 4)
+    colmap[indices[0].tolist(), indices[1].tolist(), :] = [0, 255, 0]
+    indices = np.where(label == 5)
+    colmap[indices[0].tolist(), indices[1].tolist(), :] = [0, 0, 255]
+    indices = np.where(label == 6)
+    colmap[indices[0].tolist(), indices[1].tolist(), :] = [255, 255, 255]
+    indices = np.where(label == 0)
+    colmap[indices[0].tolist(), indices[1].tolist(), :] = [0, 0, 0]
+    transform = ToTensor();
+    #     plt.imshow(colmap)
+    #     plt.show()
+    return transform(colmap)
