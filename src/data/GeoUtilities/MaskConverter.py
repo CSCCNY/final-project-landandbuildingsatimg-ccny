00e16ac7173cbf5a90aee56c9f15a381f3d7ab91
Juan Montoya <jmontoy004@citymail.cuny.edu>
2021-04-28 17:55:44 -0400

    Function to create labels for image segmentation ready

--- src/data/GeoUtilities/MaskConverter.py
+++ src/data/GeoUtilities/MaskConverter.py
@@ -1,5 +1,8 @@
+import tensorflow as tf
 import numpy as np
-
+import logging 
+from src.data.GeoUtilities import ImgSlice
+import os
 
 def RGB_mapping_to_class(label):
 #     Convert RGB mask to categorical array.
@@ -55,3 +58,51 @@ def classToRGB(label):
     #     plt.imshow(colmap)
     #     plt.show()
     return transform(colmap)
+
+
+def mask_to_label(mask_generator, directory, slices, img_height, batch_size, batch_n = None):
+# 	Transform RGB mask into labels
+#   If batch_n = None slicing will be done in all the batches of the image generator. If batch_n = int slicing will be done in the first batch_n batches.
+
+    #now we will Create and configure logger 
+    logging.basicConfig(filename="mask_to_label.log", 
+                        format='%(asctime)s - %(levelname)s - %(message)s', 
+                        filemode='w') 
+
+    #Let us Create an object 
+    logger=logging.getLogger() 
+
+    #Now we are going to Set the threshold of logger to DEBUG 
+    logger.setLevel(logging.INFO) 
+
+    logger.info(f'Lets Start \n \n \n')
+    tile_size = int(img_height/np.sqrt(slices))
+    batch_index = 0
+    logger.info(f'{os.getcwd()}')
+
+    if batch_n != None:
+        logger.info(f'It will run for {batch_n} batches \n \n \n')
+        while batch_index <= batch_n-1:
+            m = mask_generator.next()
+            for i in range(batch_size):
+                mask_tiles = ImgSlice.split_image(m[i,:,:,:], [tile_size, tile_size])
+                for t in range(mask_tiles.shape[0]):
+                    mask_label = RGB_mapping_to_class(mask_tiles[t,:,:,:])
+                    np.save(f'{directory}/Mask_Batch_{batch_index}_Image_{i}_Tile_{t}.npy', mask_label)
+                    
+            logger.info(f'Just finished batch {batch_index} \n \n \n')
+            batch_index = batch_index + 1
+            
+    else:
+        logger.info(f'It will run for all batches \n \n \n')
+        while batch_index <= data_generator.batch_index:
+            m = mask_generator.next()
+            for i in range(batch_size):
+                mask_tiles =ImgSlice.split_image(m[i,:,:,:], [tile_size, tile_size])
+                for t in range(mask_tiles.shape[0]):
+                    mask_label = RGB_mapping_to_class(mask_tiles[t,:,:,:])
+                    np.save(f'{directory}/Mask_Batch_{batch_index}_Image_{i}_Tile_{t}.npy', mask_label)
+                    
+            logger.info(f'Just finished batch {batch_index} \n \n \n')
+            batch_index = batch_index + 1
+    logger.info(f'Slicing finished')
