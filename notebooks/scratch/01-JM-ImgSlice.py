83567fb10e36a1a7f086d78592d975500d44776a
Juan Montoya <jmontoy004@citymail.cuny.edu>
2021-04-29 11:55:49 -0400

    Image saving function updated

--- notebooks/scratch/01-JM-ImgSlice.py
+++ notebooks/scratch/01-JM-ImgSlice.py
@@ -10,7 +10,7 @@ import pandas as pd
 from src.data.GeoUtilities import ImgSlice
 
 
-os.chdir('/home/juanp.montoya/NeuralNetworks/Final_Project/input')
+os.chdir('/home/juanp.montoya/NeuralNetworks/Final_Project/final-project-landandbuildingsatimg-ccny/data/raw/input')
 
 
 nRowsRead = None 
@@ -21,9 +21,10 @@ nRow, nCol = df2.shape
 img_height = 2448
 img_width = 2448
 
-data_gen = ImageDataGenerator(rescale = 1. / 255)
+data_gen = ImageDataGenerator(rescale = 1.)
+data_gen2 = ImageDataGenerator(rescale = 1.)
 
-batch_size = 50
+batch_size = 32
 
 data_generator = data_gen.flow_from_dataframe(dataframe=df2.loc[df2['split'] == 'train'], 
                                             directory="", 
@@ -34,7 +35,7 @@ data_generator = data_gen.flow_from_dataframe(dataframe=df2.loc[df2['split'] == 
                                             target_size=(img_height,img_width), 
                                             batch_size=batch_size)
 
-mask_generator = data_gen.flow_from_dataframe(dataframe=df2.loc[df2['split'] == 'train'], 
+mask_generator = data_gen2.flow_from_dataframe(dataframe=df2.loc[df2['split'] == 'train'], 
                                             directory="", 
                                             x_col="mask_path", 
                                             y_col="mask_path", 
@@ -45,5 +46,6 @@ mask_generator = data_gen.flow_from_dataframe(dataframe=df2.loc[df2['split'] == 
 
 batch_n = None
 slices = 16
+directory = '../../interim/Cropped'
 
-ImgSlice.slice_images(data_generator, mask_generator, slices, img_height, batch_size, batch_n)
+ImgSlice.slice_images(data_generator, mask_generator, directory, slices, img_height, batch_size, batch_n)
